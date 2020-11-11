72ddc8267a6b84cff4128410bb2654ae36829c79
mcollardanuy <mcollardanuy@turing.ac.uk>
2020-11-11 11:50:52 +0000

    update keep_only_19_century function

--- wikipediaprocessing/Find19thCParas.py
+++ wikipediaprocessing/Find19thCParas.py
@@ -2,6 +2,7 @@
 
 import json
 import os
+import re
 import spacy
 import dateparser
 from urllib.parse import quote
@@ -9,21 +10,77 @@ from argparse import ArgumentParser
 
 nlp = spacy.load("en_core_web_sm")
 
-matches = ["xix century","xviii century","19th century","18th century","19thc","18thc","19th c","18th c","nineteenth century","eighteenth century", "turn of the century", "fin de siècle"]
+matches = ["xix century","xviii century","19th century","18th century","19thc","18thc","19th c","18th c","nineteenth century","eighteenth century","nineteenth-century","eighteenth-century","19th-century","18th-century","early 20th c","early twentieth c"]
+
+match_decades = r'.*\b(1[789][0-9]0)s\b.*'
+match_between = r'.*between (1[789][0-9][0-9]) and (1[789][0-9][0-9]).*'
+match_fromto = r'.*from (1[789][0-9][0-9]) to (1[789][0-9][0-9]).*'
+match_around = r'.*around (1[789][0-9][0-9])\b.*'
 
 def keep_only_19_century(content):
     paras = []
     for para in content:
+        
         if any([True if match in para.lower() else False for match in matches]):
             paras.append(para)
+            ### --------------------------------
+            ### Uncomment to check which century-strings is matched in the paragraph:
+            # print(matches)
+            # print([match if match in para.lower() else False for match in matches])
+            # print(para)
+            # print()
+            ### --------------------------------
         else:
             doc = nlp(para)
-            ents = [e.text for e in doc.ents if e.label_ == "DATE" or e.label == 'TIME']
-
-            years = [dateparser.parse(x).year for x in ents if dateparser.parse(x) != None]
+            ents = [e.text for e in doc.ents if e.label_ == "DATE"]
+            
+            years = []
+            for x in ents:
+                # This regex captures decades in strings (e.g. "1860s"), strips the "s" and keeps the year.
+                if re.match(match_decades, x):
+                    years.append(re.match(match_decades, x).group(1))
+                # This regex captures between-years expressions in strings (e.g. "between 1841 and 1843"), keeps the years.
+                elif re.match(match_between, x):
+                    years += re.match(match_between, x).groups()
+                # This regex captures from-to expressions in strings (e.g. "from 1841 to 1843"), keeps the years.
+                elif re.match(match_fromto, x):
+                    years += re.match(match_fromto, x).groups()
+                # This regex captures around-years expressions in strings (e.g. "around 1841"), keeps the year.
+                elif re.match(match_around, x):
+                    years.append(re.match(match_around, x).group(1))
+                # This turns the time expression into datetime, we keep the year.
+                else:
+                    dtime = dateparser.parse(x)
+                    if dtime != None:
+                        # The following line is to avoid a preference of dateparser of turning all-numerical strings
+                        # with a lenght different from four digits into hours, minutes, and seconds, while defaulting
+                        # the year to 1900. E.g. "A666" to "1900-01-01 06:06:06", or
+                        # "886" to "1900-01-01 08:08:06", or "200000" to "1900-01-01 20:00:00".
+                        if not (x.isnumeric() and len(x) != 4 and dtime.year == 1900 and dtime.month == 1 and dtime.day == 1):
+                            years.append(dtime.year)
+                    
+                    ### --------------------------------
+                    ### Uncomment to check which DATE-strings did not return a match:
+                    #     else:
+                    #         print("==>", x) 
+                    # else:
+                    #     print("==>", x)
+                    ### --------------------------------
+                    
+            # Make sure all years are integers:
+            years = [int(year) for year in years]
             
             if any([True if 1789 <year and year < 1914 else False for year in years]):
                 paras.append(para)
+                
+                ### --------------------------------
+                ### Uncomment to check which years are matched in the paragraphs:
+                # print(years)
+                # print([year if 1789 <year and year < 1914 else False for year in years])
+                # print(para)
+                # print()
+                ### --------------------------------
+                
     if len(paras)>0:
         return paras
 
@@ -68,7 +125,7 @@ if __name__ == "__main__":
         os.mkdir("outputs")
 
     if os.path.exists("outputs/"+entity+".txt"):
-        os.remove(entity+".txt")
+        os.remove("outputs/"+entity+".txt")
 
     for k,v in sorted_sections.items():
         print (k)
