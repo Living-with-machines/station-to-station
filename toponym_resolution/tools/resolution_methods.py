ad2ca06813d23ae027c4c74fd98115805a793c8c
Federico Nanni <nanni.federico@gmail.com>
2021-04-15 16:07:19 +0000

    Add ranklib integration in resolution notebook

--- toponym_resolution/tools/resolution_methods.py
+++ toponym_resolution/tools/resolution_methods.py
@@ -9,6 +9,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, f1_score
 from urllib.parse import quote
 import random
+import pathlib
+import subprocess
 
 from sentence_transformers import SentenceTransformer, util
 model = SentenceTransformer('paraphrase-distilroberta-base-v1')
@@ -332,5 +334,83 @@ def skyline(features_df, test_df):
     
     return test_df
 
-        
 
+# ----------------------------------
+# RankLib
+# ----------------------------------
+
+def convert_feature_file_format(partition,features, filter):
+
+    if filter == "exact":
+        outfile = partition+"_exact-ranklib.tsv"
+    elif filter == "notexact":
+        outfile = partition+"_notexact-ranklib.tsv"
+    else:
+        outfile = partition+"_all-ranklib.tsv"
+
+    out_feat_folder = "supervised_ranking/feature_files/"
+    out_model_folder = "supervised_ranking/models/"
+    pathlib.Path(out_feat_folder).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(out_model_folder).mkdir(parents=True, exist_ok=True)
+    out = open(out_feat_folder+ outfile,"w")
+# MainId	SubId	Query	Candidate	f_0	f_1	f_2	f_3	f_4	f_5	f_6	f_7	f_8	Label	Exact
+    current_qid = 0
+    current_sub_id = 0
+    for _id,line in features.iterrows():
+        feat_vect = []
+        filter_flag = line["Exact"]
+        if filter == "exact" and filter_flag!=1:
+            continue
+        if filter == "notexact" and filter_flag!=0:
+            continue
+
+        label = line["Label"]
+        cand = line["Candidate"]
+        feat_vect.append(str(label))
+        sub_id = line ["SubId"]
+        if sub_id != current_sub_id:
+            current_sub_id = sub_id
+            current_qid+=1
+        feat_vect.append("qid:"+str(current_qid))
+        feat_counter = 1
+        for f in range(5,14):
+            feat_vect.append(str(feat_counter)+":"+str(line.iloc[f]))
+            feat_counter+=1
+        feat_vect.append("# "+ str(cand)+ " "+ str(current_sub_id))
+        out.write(" ".join(feat_vect))
+        out.write("\n")
+    out.close()
+    return out_feat_folder+ outfile
+
+def ranklib(dev_feat_file,test_feat_file,filter,code_folder,cross_val,test_df):
+
+    dev = convert_feature_file_format("dev",dev_feat_file,filter=filter)
+    feature_used = open("supervised_ranking/models/features.txt","r").read().replace('\n'," ")
+
+    if cross_val == True:
+        print ("feature used:",feature_used)
+        out =  subprocess.check_output(["java", "-jar",code_folder+"PlaceLinking/toponym_resolution/tools/RankLib-2.13.jar","-train",dev,"-ranker","4","-kcv", "5", "-metric2t","P@1", "-metric2T", "P@1","-feature","supervised_ranking/models/features.txt"])
+        out = out.decode("utf-8").split("\n")[-15:]
+        return out
+    else:
+        test = convert_feature_file_format("test",test_feat_file,filter="all")
+        out = subprocess.check_output(["java", "-jar",code_folder+"PlaceLinking/toponym_resolution/tools/RankLib-2.13.jar","-train",dev,"-test",test,"-ranker","4","-metric2t","P@1", "-metric2T", "P@1","-save",code_folder+"PlaceLinking/toponym_resolution/supervised_ranking/models/model.run","-feature","supervised_ranking/models/features.txt"])
+        train_performance = out.decode("utf-8").split("\n")[-6]
+        test_performance = out.decode("utf-8").split("\n")[-4]
+        print (train_performance,test_performance)
+        subprocess.check_output(["java", "-jar",code_folder+"PlaceLinking/toponym_resolution/tools/RankLib-2.13.jar","-load","supervised_ranking/models/model.run","-rank",test,"-indri","supervised_ranking/models/rank.txt","-feature","supervised_ranking/models/features.txt"])
+        
+        rank = open("supervised_ranking/models/rank.txt","r").read().strip().split("\n")
+        q_ids = set([int(x.split(" ")[3]) for x in rank])
+
+        results = {}
+
+        for q_id in q_ids:
+            scores = [[x.split(" ")[2],float(x.split(" ")[5])] for x in rank if int(x.split(" ")[3])==q_id]
+            scores.sort(key=lambda x: x[1],reverse=True)
+            results[q_id] = scores[0][0]
+        print ("feature used:",feature_used)
+
+    test_df["ranklib"] = test_df['SubId'].map(results)
+    
+    return test_df
